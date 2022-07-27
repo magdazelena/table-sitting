cb8c16d97bb06559d972b311f7767a4e3f49fa26
magdazelena <magda.zelezik@gmail.com>
2022-07-27 20:53:01 +0200

    temp: logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import logic from "./logic";
 function App() {
+  const guestList = [1, 2, 1, 3, 4, 4, 1, 2, 3, 4, 5, 6, 7, 6]
+  //assumptions:
+  // -same numbers together
+  // - exclusions can't be next to one another
+  // - groups may or may not be together
+  // asumptions have priority as listed
+
+  const exclusions = [[3, 4]];
+  const groups = [
+    [1, 4, 3, 6],
+    [2, 5]
+  ];
   return (
     <div>
-
+      <div>Main list: {guestList}</div>
+      <div>Output: {logic(guestList, groups, exclusions).map((item) => {
+        return <div>{item}</div>
+      })}</div>
     </div>
   );
 }
--- src/logic/index.js
+++ src/logic/index.js
@@ -1,32 +1,62 @@
-const mainList = [1, 2, 1, 3, 4, 4, 1, 2, 3, 4, 5, 6, 7, 6];
-//assumptions:
-// -same numbers together
-// - exclusions can't be next to one another
-// - groups may or may not be together
-// asumptions have priority as listed
+import _ from 'lodash';
 
-const exclusions = [[3, 4]];
-const groups = [
-  [1, 4, 3, 6],
-  [2, 5]
-];
-//first let's group
-let mainListCopy = [...mainList];
-const groupedLists = [];
-for (const group of groups) {
-  const tempList = _.filter(mainList, (i) => group.includes(i));
-  mainListCopy = _.difference(mainListCopy, group);
-  groupedLists.push(tempList);
-}
-groupedLists.push(mainListCopy);
-//let's exclude now
-//1. let's pick a group that contains exclusions and skip the rest
-groupedLists.forEach((list) => {
-  if (_.intersection(list, ...exclusions).length !== 2) return;
-  exclusions.forEach((elist) => {
-    console.log(list, "presort");
-    list = sortNeighbors(list, elist);
-    console.log(list);
+export default function calculateTables(mainList, groups, exclusions) {
+  //first let's group
+  let mainListCopy = [...mainList];
+  const groupedLists = [];
+  for (const group of groups) {
+    const tempList = _.filter(mainList, (i) => group.includes(i));
+    mainListCopy = _.difference(mainListCopy, group);
+    groupedLists.push(tempList);
+  }
+  groupedLists.push(mainListCopy);
+  //let's exclude now
+  //1. let's pick a group that contains exclusions and skip the rest
+  groupedLists.forEach((list) => {
+    list.sort()
+    if (_.intersection(list, ...exclusions).length !== 2) return list;
+    //find last index of exclusion
   });
-});
+  return groupedLists
+}
 
+
+function interfere(list, exclusions) {
+  //find indexes of possible exclusions
+  let indexes = getIndexes(list, exclusions)
+  do {
+    //interefring
+    const i = findIndexOfFirstConsecutivePair(indexes)
+    const items = mapItemsToKeys(list)
+
+
+  } while (isConsecutive(indexes))
+  return list
+}
+
+function findIndexOfFirstConsecutivePair(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1] - 1) return array[i]
+  }
+}
+
+function isConsecutive(array) {
+  return array.reduce((output, lastest) => (output ? (output + 1 === lastest ? lastest : false) : false));
+}
+
+function getIndexes(source, array) {
+  return array.map(item => source.indexOf(item))
+}
+function mapItemsToKeys(array) {
+  const object = []
+  for (let i = 0; i < array.length; i++) {
+    if (Object.keys(object).includes(array[i])) {
+      object[array[i]].push(i)
+    } else {
+      object.push({
+        [array[i]]: [i]
+      })
+    }
+  }
+  return object
+}
